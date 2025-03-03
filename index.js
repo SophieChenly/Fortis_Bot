fdc97715161841fbc2764fbac57bdebd3668b7ca
Sophie Chen <sophiechenly@gmail.com>
2025-03-02 20:35:48 -0800

    Intergrated the new time string generator. Gitignore. Env.

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ client.once('ready', () => {
 
             // Format the time to show only the hour with AM/PM
             const currentTime = moment().tz("America/Los_Angeles").format('h A');
-            await channel.setName(`🕒 ${currentTime}`);
-            console.log(`Updated channel name to: ${currentTime}`);
+            const timeString = getTimeString(currentTime);
+            await channel.setName(timeString);
+            console.log(`Updated channel name to: ${timeString}`);
         } catch (error) {
             console.error("Error updating channel:", error);
         }
@@ -29,41 +30,4 @@ client.once('ready', () => {
     setInterval(updateChannelName, 60 * 60 * 1000);
 });
 
-// async function getChannelName(currTime) {
-//     const split = currTime.split(" ");
-//     const time = parseInt(split[0]);
-//     const a = split[1];
-    
-//     // sunrise + sunset 🌄 (6am/pm)
-//     // day 🏞 (7am-5pm) -> 7-11am + 12-5pm
-//     // night 🌌 (7pm-5am) -> 7-11pm + 12-5am
-
-//     if (time == 6) {
-//         return `🌄 ${currTime}`;
-//     } else if (time <= 11 && time >= 7 ) {
-//         if (a == "AM") {
-//             return `🏞 ${currTime}`;
-//         } else if (a == "PM") {
-//             return `🌌 ${currTime}`;
-//         } else {
-//             console.error("Error parsing AM/PM");
-//             return "error";
-//         }
-//     } else if (time == 12 || time <= 5) {
-//         if (a == "AM") {
-//             return `🌌 ${currTime}`;
-//         } else if (a == "PM") {
-//             return `🏞 ${currTime}`;
-//         } else {
-//             console.error("Error parsing AM/PM");
-//             return "error";
-//         }
-//     } else {
-//         console.error("Error parsing time: " + currTime);
-//         return "error";
-//     }
-// }
-
 client.login(process.env.TOKEN);
-
-module.exports = getChannelName;
